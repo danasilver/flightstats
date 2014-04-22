2436b7d65eb460e9c6ad788e9b0b114af0b2594b
danasilver <dsilver1221@gmail.com>
2014-04-22 13:57:01 -0400

    Support all urls.

--- README.md
+++ README.md
@@ -4,10 +4,6 @@ A CORS enabled wrapper on the FlightStats API.
 
 Created for use on a project for [Middlebury College CS465](go.middlebury.edu/infovis) requiring AJAX access to the FlightStats API.
 
-### Support
-
-Currently only supports scheduled flights by route, departing on a given date, detailed [here](https://developer.flightstats.com/api-docs/scheduledFlights/v1).
-
 ### Use
 
 Use the same exact URL as with FlightStats, only with `flightstats-api.herokuapp.com` prepended in place of `api.flightstats.com`.
@@ -15,11 +11,13 @@ Use the same exact URL as with FlightStats, only with `flightstats-api.herokuapp
 Example:
 
 ```js
-var url = "http://flightstats-api.herokuapp.com/flex/schedules/rest/v1/json/from/PHL/to/BTV/departing/2014/4/22?appId=APPID&appKey=APPKEY",
+var url = "http://flightstats-api.herokuapp.com/flex/airports/rest/v1/json/cityCode/ABC?appId=APPID&appKey=APPKEY",
     xhr = new XMLHttpRequest();
 
 xhr.open("GET", url, true);
 xhr.send(null);
 ```
+
+HTTP and HTTPS protocols are supported.
 
 You still need an `appId` and `appKey`!
--- app.js
+++ app.js
@@ -3,30 +3,26 @@ var express = require('express'),
 
 var app = express();
 
-var flightstats_routes_url = 'https://api.flightstats.com/flex/schedules/rest/v1/json';
+var flightstats_url = 'https://api.flightstats.com';
 
-app.get('/flex/schedules/rest/v1/json/from/:from/to/:to/departing/:y/:m/:d', function(req, res) {
+app.get('/', function(req, res) {
+  res.send(200, '<p>See documentation for API usage.<p>' +
+                '<p><a href="https://github.com/danasilver/flightstats">Documentation for this project</a></p>' +
+                '<p><a href="https://developer.flightstats.com/api-docs/">FlightStats Documentation</a></p>')
+});
+
+app.get('*', function(req, res) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
 
-  var request_url = flightstats_routes_url +
-                    '/from/' + req.param('from') +
-                    '/to/' + req.param('to') +
-                    '/departing/' + req.param('y') +
-                    '/' + req.param('m') +
-                    '/' + req.param('d') + 
-                    '?appId=' + req.param('appId') +
-                    '&appKey=' + req.param('appKey');
-
-  console.log(request_url);
-
-  request.get(request_url, function(err, r_res, body) {
+  request.get(flightstats_url + req.originalUrl, function(err, r_res, body) {
     if (err) res.send(500);
-    else res.send(JSON.parse(body));
+    else if (r_res.statusCode === 200) res.send(JSON.parse(body));
+    else res.send(body);
   });
 }); 
 
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
   console.log('Listening on ' + port + '.');
-})
+});
